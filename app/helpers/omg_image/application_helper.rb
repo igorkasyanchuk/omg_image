37ff0fe138de28476db8040e14b6a812999051ec
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2018-11-03 05:30:55 -0700

    working on new version

--- app/helpers/omg_image/application_helper.rb
+++ app/helpers/omg_image/application_helper.rb
@@ -1,6 +1,8 @@
 require 'uri'
 require 'tempfile'
 require 'open3'
+require 'timeout'
+require "open4"
 
 class BetterTempfile < Tempfile
   # ensures the Tempfile's filename always keeps its extension
@@ -16,47 +18,71 @@ module OmgImage
   module ApplicationHelper
     include OmgHelper
 
-    def omg_image_preview(omg: {})
-      omg        ||= {}
-      omg[:size] ||= '800,400'  
-      omg[:key]  ||= omg.to_s
+    def omg_image_preview(options)
+      options        ||= {}
+      options[:size] ||= '800,400'  
+      options[:key]  ||= options.to_s
 
-      image       = OmgImage::Image.find_by(key: omg[:key])
+      image       = OmgImage::Image.find_by(key: options[:key])
 
       if image && !image.file.attached?
         image.destroy
         image = nil
       end
 
-      file = image&.file || create_screenshot(omg)
+      file = image&.file || create_screenshot(options)
       file ?  url_for(file) : "something-went-wrong.png"
     end
 
     private
 
-    def create_screenshot(omg)
+    def create_screenshot(options)
       begin
         file = BetterTempfile.new("image.png")
 
         options = {
           file: file,
-          size: omg[:size],
-          url: omg_image.preview_url(omg: omg)
+          size: options[:size],
+          url: omg_image.preview_url(omg: options)
         }
+
+        binding.pry
+
+        input = ApplicationController.render file: "og_image/previews/show"
 
         command = build_chrome_command(options)
         Rails.logger.debug "  ====>> executing: #{command}"
 
-        err = Open3.popen3(*command) do |_stdin, _stdout, stderr|
-          puts "stdout: #{_stdout.read}"
-          stderr.read
-        end
+        #Timeout::timeout(5) {
+          # err = Open3.popen3(*command) do |_stdin, _stdout, stderr|
+          #   puts "stdout: #{_stdout.read}"
+          #   stderr.read
+          # end
+          # if err.present?
+          #   Rails.logger.debug "  ===>> error: #{err}"
+          # else
+          #   Rails.logger.debug "Attaching ...."
+          # end
+          # err = Open4::popen4(command) do |pid, stdin, stdout, stderr|
+          #   Rails.logger.info "pid: #{pid}"
+          #   stderr.read
+          # end
 
-        if err.present?
-          Rails.logger.debug "  ===>> error: #{err}"
-        end
+          begin
+            process = open4.spawn command, timeout: 5
+            pid = process.pid
+            Rails.logger.info "==========="
+            Rails.logger.info process
+            Rails.logger.info process.pid
+            Rails.logger.info "==========="
+          rescue Timeout::Error
+            Rails.logger.info "++++++++#{process}+++++#{pid}++++"
+            Process.kill('KILL', process.pid)
+            Rails.logger.info "++++++++"
+          end
+        #}
 
-        image = OmgImage::Image.find_or_create_by(key: omg[:key])
+        image = OmgImage::Image.find_or_create_by(key: options[:key])
 
         if !image.file.attached?
           image.file.attach(io: File.open(file.path), filename: "image.png", content_type: "image/png")
--- omg_image.gemspec
+++ omg_image.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency "rails", "~> 5.2.1"
   s.add_dependency "gastly"
+  s.add_dependency "open4"
 
   s.add_development_dependency "sqlite3"
   s.add_development_dependency "puma"
